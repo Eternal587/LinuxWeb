6b6cfaf8429a74718b996881d624afbfd9830af3
Chiller Cat <81083840+Eternal587@users.noreply.github.com>
2022-10-15 14:37:48 -0400

    Update RunLinux.js

--- RunLinux.js
+++ RunLinux.js
@@ -1,1 +1,9 @@
-Prompt("do you have an old version")
+var Savefile = "undefined";
+let Savefile = Prompt("do you have an old save file (y or n)");
+
+if (Savefile == "n") {
+  let b = prompt("i work i think")
+}
+if (Savefile == "y") {
+let a = prompt("wow i work")
+}
